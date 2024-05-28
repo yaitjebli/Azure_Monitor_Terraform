636ae9426574435dd1d1556ceb67eb10b48ab6f3
Younes <y.aitjebli@gmail.com>
2024-05-28 18:15:50 +0200

    Linux VM 'LX-VM2' adding'

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ provider "azurerm" {
     }
 }
 
-
+# Création de la resource group "rg-alpha"
 resource "azurerm_resource_group" "rglog" {
     name = "rg-alpha"
     location = "West Europe"
 }
 
-
+# Création du compte de stockage "disklog"
 resource "azurerm_storage_account" "disklog" {
     name = "diskdelog"
     resource_group_name = azurerm_resource_group.rglog.name
@@ -19,7 +19,7 @@ resource "azurerm_storage_account" "disklog" {
     account_replication_type = "LRS"
 }
 
-
+# Création du réseau virtuel "netwin"
 resource "azurerm_virtual_network" "netlog" {
     name = "netwin"
     location = azurerm_resource_group.rglog.location
@@ -27,7 +27,7 @@ resource "azurerm_virtual_network" "netlog" {
     address_space = [ "192.168.0.0/16" ]
 }
 
-
+# Création du sous-réseau virtuel subnetwin
 resource "azurerm_subnet" "subnetlog" {
     name = "subnetwin"
     resource_group_name = azurerm_resource_group.rglog.name
@@ -35,7 +35,7 @@ resource "azurerm_subnet" "subnetlog" {
     address_prefixes = [ "192.168.1.0/24" ] 
 }
 
-
+# Création de l'interface réseau "wincard"
 resource "azurerm_network_interface" "logcard" {
     name = "wincard"
     location = azurerm_resource_group.rglog.location
@@ -45,12 +45,10 @@ resource "azurerm_network_interface" "logcard" {
         name = "internalip"
         subnet_id = azurerm_subnet.subnetlog.id
         private_ip_address_allocation = "Dynamic"
-
     }
-  
 }
 
-
+# Création de la machine virtuelle "WS-VM1"
 resource "azurerm_windows_virtual_machine" "vmlog" {
     name = "WS-VM1"
     location = azurerm_resource_group.rglog.location
@@ -73,4 +71,43 @@ resource "azurerm_windows_virtual_machine" "vmlog" {
         sku = "2016-Datacenter"
         version = "latest"
     }
+}
+
+# Création de l'interface réseau "lincard"
+resource "azurerm_network_interface" "loglincard" {
+    name = "lincard"
+    location = azurerm_resource_group.rglog.location
+    resource_group_name = azurerm_resource_group.rglog.name
+
+    ip_configuration {
+        name = "internalip"
+        subnet_id = azurerm_subnet.subnetlog.id
+        private_ip_address_allocation = "Dynamic"
+    }
+}
+
+# Création de la machine virtuelle "LX-VM2"
+resource "azurerm_linux_virtual_machine" "vm2" {
+  name                = "LX-VM2"
+  location            = azurerm_resource_group.rglog.location
+  resource_group_name = azurerm_resource_group.rglog.name
+  size                = "Standard_B2ms"
+  admin_username      = "Prime"
+  admin_password      = "P@sswordP@ssword"
+  network_interface_ids = [
+    azurerm_network_interface.loglincard.id
+  ]
+  disable_password_authentication = false
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
 }
